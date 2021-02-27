e8769003b8d363d2fdeebad6722eb4ad72024963
HelenaIsh <elena-ismatova@mail.ru>
2021-02-27 23:27:36 +0500

    tasks 1-7

--- userProfile/src/index.tsx
+++ userProfile/src/index.tsx
@@ -1,4 +1,8 @@
 import './style.css';
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Button, Input, Select, Gapped, Modal } from '@skbkontur/react-ui';
+import './style.css';
 
 /**
  *  Итак, перед тобой пустой проект. Давай его чем-то заполним. Не стесняйся подсматривать в уже сделанные задачи,
@@ -55,5 +59,117 @@ import './style.css';
  *      гражданство, национальность, номер телефона и адрес электронной почты.
  *      Придумай, как избежать излишнего дублирования.
  */
+const prevUser: { name?: string; surname?: string; city?: string } = {};
 
-console.log('Hi from script!');
+class Form extends React.Component {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            name: '',
+            surname: '',
+            city: 'Екатеринбург',
+            opened: false
+        };
+        this.renderModal = this.renderModal.bind(this);
+        this.open = this.open.bind(this);
+        this.close = this.close.bind(this);
+    }
+
+    renderModal() {
+        let modalHeader = '';
+        let str = '';
+        let name = '';
+        let surname = '';
+        let city = '';
+        let modalBody;
+        if (Object.keys(prevUser).length === 0) {
+            modalHeader = 'Пользователь сохранен';
+        } else {
+            str = 'Измененные данные: ';
+            modalHeader = 'Пользователь изменен';
+            if (prevUser.name !== this.state.name) {
+                name += `Имя: было: ${prevUser.name}, стало: ${this.state.name}`;
+            }
+            if (prevUser.surname !== this.state.surname) {
+                surname += `Фамилия: было: ${prevUser.surname}, стало: ${this.state.surname}`;
+            }
+            if (prevUser.city !== this.state.city) {
+                city += `Город: было: ${prevUser.city}, стало: ${this.state.city}`;
+            }
+            modalBody = <Modal.Body>
+                <p>{str}</p>
+                <p>{name}</p>
+                <p>{surname}</p>
+                <p>{city}</p>
+            </Modal.Body>
+        }
+        prevUser.name = this.state.name;
+        prevUser.surname = this.state.surname;
+        prevUser.city = this.state.city;
+        return (
+            <Modal onClose={this.close}>
+                <Modal.Header>{modalHeader}</Modal.Header>
+                {modalBody}
+                <Modal.Footer>
+                    <Button onClick={this.close}>Закрыть</Button>
+                </Modal.Footer>
+            </Modal>
+        );
+    }
+
+    open() {
+        this.setState({ opened: true });
+    }
+
+    close() {
+        this.setState({ opened: false });
+    }
+
+    render() {
+        const items = ['Екатеринбург', 'Москва', 'Санкт-Петербург', 'Казань'];
+        return (
+            <div className="Form-container">
+                {this.state.opened && this.renderModal()}
+                <h1 className="Form-header">Информация о пользователе</h1>
+                <form className="Form">
+                    <Gapped vertical gap={20}>
+                        <div className="Form-group">
+                            <label className="label" htmlFor="name">
+                                Имя
+                            </label>
+                            <Input
+                                id="name"
+                                placeholder="Введите имя пользователя"
+                                onValueChange={value => this.setState({name: value})}
+                            />
+                        </div>
+                        <div className="Form-group">
+                            <label className="label" htmlFor="surname">
+                                Фамилия
+                            </label>
+                            <Input
+                                id="surname"
+                                placeholder="Введите фамилию пользователя"
+                                onValueChange={value => this.setState({surname: value})}
+                            />
+                        </div>
+                        <div className="Form-group">
+                            <label className="label" htmlFor="city">
+                                Город
+                            </label>
+                            <Select
+                                placeholder="Выберите город"
+                                items={items}
+                                value={this.state.city}
+                                onValueChange={value => this.setState({ city: value })}
+                            />
+                        </div>
+                        <Button onClick={this.open}>Сохранить</Button>
+                    </Gapped>
+                </form>
+            </div>
+        );
+    }
+}
+
+ReactDom.render(<Form />, document.getElementById('app'));
