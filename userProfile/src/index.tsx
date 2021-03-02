ecd5b01960ce53972bb0e553706b9584c0416501
Mayrinika <mashuliy_u@mail.ru>
2021-03-02 19:51:11 +0500

    tasks 1-7

--- userProfile/src/index.tsx
+++ userProfile/src/index.tsx
@@ -1,59 +1,144 @@
 import './style.css';
+import React from 'react';
+import ReactDom from 'react-dom';
+import {Button, Input, Select, Gapped, Modal} from '@skbkontur/react-ui';
 
-/**
- *  Итак, перед тобой пустой проект. Давай его чем-то заполним. Не стесняйся подсматривать в уже сделанные задачи,
- *  чтобы оттуда что-то скопировать.
- *
- *  1. Создай в файле index.html (он на уровень выше в файловой структуре) div с каким-нибудь id
- *  2. Импортируй сюда библиотеку React и ReactDom
- *  3. Отрендери "Hello world" на странице при помощи Реакта.
- *
- *  4. Добавь в разметку Button из библиотеки компонентов Контура (@skbkontur/react-ui).
- *     npm-пакет с библиотекой уже добавлен в проект.
- *
- *     Импортируется компонент на страницу так:
- *
- *     import Button from '@skbkontur/react-ui/Button';
- *
- *     Используется компонент так:
- *
- *     const MyApp = () => (
- *        <div>
- *            Click this button <Button onClick={() => console.log('Hey!')}>Click me</Button>
- *        </div>
- *     );
- *
- *
- *     Тут можно посмотреть, как компонентами пользоваться, какие у них атрибуты есть:
- *         http://tech.skbkontur.ru/react-ui/
- *
- *  5. Теперь, когда ты знаешь все основное, сверстай при помощи react-ui форму,
- *     как на картинке drafts/reactForm.png (можно открыть в браузере http://localhost:8080/drafts/reactForm.png)
- *     Для вертикальных отступов между элементами используй компонент Gapped из библиотеки.
- *     Если хочешь что-то стилизовать, используй файл style.css.
- *     Список городов придумай сам.
- *
- *  6. Сделай так, чтобы при клике по кнопке "Сохранить",
- *     показывалось модальное окно (компонент Modal из библиотеки) с текстом "Пользователь сохранен".
- *     выглядеть будет примерно как на картинке drafts/reactModal.png (http://localhost:8080/drafts/reactModal.png)
- *
- *  7. Сделай так, чтобы в той же модалке (в теле модального окна — Modal.Body) показывались изменения в полях.
- *     Смотри drafts/reactDiff.png (http://localhost:8080/drafts/reactDiff.png).
- *     Пример сообщения:
- *
- *       Измененные данные:
- *       Фамилия: было "Петров", стало "Петрова"
- *
- *     Для этого надо хранить где-то предыдущее (и текущее) значение. Придумай, как лучше это сделать.
- *
- *  8*. Необязательная задача.
- *      Сделай так, чтобы форма не сохранялась, если поле имя или фамилия не заполнено.
- *      Незаполненное поле должно анимацией покачаться из стороны в сторону (или придумай любой другой эффект).
- *
- *  9*. Необязательная задача.
- *      Добавь в эту форму еще поля: пол, дата рождения, город рождения, семейное положение,
- *      гражданство, национальность, номер телефона и адрес электронной почты.
- *      Придумай, как избежать излишнего дублирования.
- */
+type FormData = {
+    name: string;
+    surname: string;
+    city: string;
+}
 
-console.log('Hi from script!');
+const defaultFormData = {
+    name: '',
+    surname: '',
+    city: ''
+}
+
+type CreateFormProps = {}
+
+type CreateFormState = {
+    isModalOpened: boolean;
+    savedData: FormData;
+    currentData: FormData;
+}
+
+class CreateForm extends React.Component<CreateFormProps, CreateFormState> {
+    constructor(props: CreateFormProps) {
+        super(props);
+        this.state = {
+            isModalOpened: false,
+            savedData: {...defaultFormData},
+            currentData: {...defaultFormData},
+        }
+    }
+
+    renderChangeData(field: keyof FormData) {
+        const currentData = this.state.currentData;
+        const savedData = this.state.savedData;
+
+        return (savedData[field] !== currentData[field] &&
+            <div>{field}: было {savedData[field]}, стало {currentData[field]}</div>);
+    }
+
+    renderModal() {
+        return (
+            <Modal onClose={this.closeModalAndSaveData}>
+                <Modal.Header>Пользователь сохранен</Modal.Header>
+                <Modal.Body>
+                    <div>
+                        Измененные данные:
+                        {this.renderChangeData('name')}
+                        {this.renderChangeData('surname')}
+                        {this.renderChangeData('city')}
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={this.closeModalAndSaveData}>Закрыть</Button>
+                </Modal.Footer>
+            </Modal>
+        );
+    }
+
+    closeModalAndSaveData = () => {
+        this
+            .setState({
+                isModalOpened: false,
+                savedData: {...this.state.currentData}
+            });
+    }
+
+    openModal = () => {
+        this.setState({
+            isModalOpened: true
+        });
+    }
+
+    changeFormData(value: string, field: string) {
+        this.setState({
+            currentData: {
+                ...this.state.currentData,
+                [field]: value,
+            }
+        })
+    }
+
+    renderForm() {
+        return (
+            <form>
+                <h2>Информация о пользователе</h2>
+                <Gapped gap={15} vertical>
+                    <div className='row'>
+                        <label htmlFor='name'>
+                            <div className='label'>Имя</div>
+                            <Input
+                                type='text'
+                                id='name'
+                                placeholder='Введите имя пользователя'
+                                onValueChange={value => this.changeFormData(value, 'name')}
+                            />
+                        </label>
+                    </div>
+                    <div className='row'>
+                        <label htmlFor='surname'>
+                            <div className='label'>Фамилия</div>
+                            <Input
+                                type='text'
+                                id='surname'
+                                placeholder='Введите фамилию пользователя'
+                                onValueChange={value => this.changeFormData(value, 'surname')}
+                            />
+                        </label>
+                    </div>
+                    <div className='row'>
+                        <label>
+                            <div className='label'>Город</div>
+                            <Select<string>
+                                items={['Екатеринбург', 'Тюмень', 'Челябинск', 'Курган']}
+                                placeholder='Выберите город'
+                                onValueChange={value => this.changeFormData(value, 'city')}
+                            />
+                        </label>
+                    </div>
+                    <Button use="primary" onClick={this.openModal}>Сохранить</Button>
+                </Gapped>
+            </form>
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderForm()}
+                {this.state.isModalOpened && this.renderModal()}
+            </div>
+        );
+    }
+}
+
+
+ReactDom.render(
+    <CreateForm/>,
+    document.getElementById('app')
+);
+
