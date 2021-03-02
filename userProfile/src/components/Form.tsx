3e5d1099803988730fb851d9a6aefa5c3b2c77a0
MariaChumerina <masha.chumerina@gmail.com>
2021-03-02 19:00:47 +0500

    add layout

--- userProfile/src/components/Form.tsx
+++ userProfile/src/components/Form.tsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { cities } from '../data/cities';
+import { Gapped, Input, Select } from "@skbkontur/react-ui";
 
 export const Form: React.FC = () => {
-    return <div>form</div>;
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [city, setCity] = useState('');
+
+    const getCities = () => cities.map(city => city.title);
+
+    return (
+        <form>
+            <h2>Информация о пользователе</h2>
+            <Gapped vertical gap={20}>
+                <div>
+                    <label htmlFor="name" className="label">
+                        Имя
+                    </label>
+                    <Input id="name" type="text" value={name} />
+                </div>
+                <div>
+                    <label htmlFor="surname" className="label">
+                        Фамилия
+                    </label>
+                    <Input id="surname" type="text" value={surname} />
+                </div>
+                <div>
+                    <label htmlFor="city" className="label">
+                        Город
+                    </label>
+                    <Select items={getCities()} value={city} placeholder={'Выберите город'} />
+                </div>
+            </Gapped>
+        </form>
+    );
 };
---
+++ userProfile/src/data/cities.js
@@ -0,0 +1,5 @@
+export const cities = [
+    { id: 1, title: 'Москва' },
+    { id: 2, title: 'Санкт-Петербург' },
+    { id: 3, title: 'Екатеринбург' }
+];
