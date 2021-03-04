9ff8eaf4432d490cb3892043e83b67cc02af9e9e
vabyars <vasiliyrait@gmail.com>
2021-03-04 17:30:42 +0500

    Form done(1-7)

--- userProfile/src/index.tsx
+++ userProfile/src/index.tsx
@@ -1,4 +1,7 @@
 import './style.css';
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Button, Input, Gapped, Select, Modal } from '@skbkontur/react-ui';
 
 /**
  *  Итак, перед тобой пустой проект. Давай его чем-то заполним. Не стесняйся подсматривать в уже сделанные задачи,
@@ -56,4 +59,137 @@ import './style.css';
  *      Придумай, как избежать излишнего дублирования.
  */
 
-console.log('Hi from script!');
+
+type FormData = {
+  name : string,
+  surname : string,
+  city : string
+}
+
+type FormState = {
+  isModalOpen : boolean,
+  current: FormData,
+  saved: FormData
+}
+
+
+const defoultData = {
+  name: " ",
+  surname: " ",
+  city: "Yekaterinburg"
+}
+
+const cities = ["Yekaterinburg", "Moscow", "Tokyo", "London", "Paris"]
+
+class Form extends React.Component<{}, FormState>{
+
+  constructor(props: FormState){
+   super(props)
+   this.state = {
+    current : { ...defoultData },
+    isModalOpen : false,
+    saved : {...defoultData}
+    }
+  }
+  
+  render(){
+    return <form>
+    <div className="form">
+      <Gapped vertical gap={30}>
+        <p className="title">User information</p>
+      </Gapped>
+      <Gapped vertical gap={15}>
+        { this.GetInputLable("Name", "Enter your name", this.currentUpdateHandler, "name") }
+        { this.GetInputLable("Surname", "Enter your surname", this.currentUpdateHandler, "surname") }
+        { this.GetSelectLable("City", cities , this.currentUpdateHandler, "city") }
+        <div className="buttonwrap">
+          <Button use="pay" size="medium"  onClick={() => this.openModal()}>Save</Button>
+        </div>
+      </Gapped>
+      {this.state.isModalOpen && this.CreateModal()}
+    </div>
+    </form>
+  }
+
+  private currentUpdateHandler = (value: string, field: keyof FormData) =>{
+    this.setState({ current: {...this.state.current, [field]: value}})
+  }
+
+  private GetSelectLable(lableText: string, selectData: string[], 
+    handler: (value: string, field: keyof FormData) => void,
+     field: keyof FormData){
+    return <label>
+        <div className="label">{lableText}</div> 
+        <Select<string> use="success" value={this.state.current.city} items={selectData} onValueChange={value => handler(value, field)} ></Select>     
+    </label>  
+  }
+
+  private GetInputLable(lableText: string, inputShadowText: string,
+    handler: (value: string, field: keyof FormData) => void,
+     field: keyof FormData): JSX.Element{
+   return <label>
+     <div className="label">{lableText}</div> 
+      <Input  placeholder={inputShadowText} onChange={(event) => handler(event.target.value, field) }/> 
+   </label>                
+  }
+
+
+  private CreateModal(){
+     return (
+     <Modal onClose={() => this.closeModal()} >
+       <Modal.Header>User has been saved</Modal.Header>
+       <Modal.Body>{this.GetComparationText()}</Modal.Body>
+       <Modal.Footer>
+         <Button use="pay" size="medium" onClick={() => this.closeModal()}>Close</Button>
+       </Modal.Footer>
+     </Modal>
+     );
+  }
+
+  private closeModal(){
+    this.setState({
+      isModalOpen: false,
+      saved: { ...this.state.current }
+    })    
+  }
+
+  private openModal(){
+    this.setState({isModalOpen: true})
+  }
+
+  private GetComparationText(){
+    let changes = this.getChangesList(['name', 'surname', 'city'])
+    let wasChanged = changes.some(fragment => fragment)
+    return wasChanged 
+    && <Gapped vertical gap={10}>
+        {"Changes:"} 
+        {changes}  
+      </Gapped>
+  }
+
+  private getChangesList(fields: (keyof FormData)[]){
+    let result = []
+    for (const field of fields)
+      result.push(this.getComparation(field))
+    return result
+  }
+
+  private getComparation(field: keyof FormData){
+    let current = this.state.current
+    let saved = this.state.saved
+    let text = current[field] !== saved[field] 
+      ? `${saved[field]} => ${current[field]}`
+      : null
+    return text != null && <React.Fragment key={field}>
+      {`${this.upFirstLetter(field)}: ${text} `} 
+     </React.Fragment>       
+  }
+
+  private upFirstLetter = (str: string) => str ? str[0].toUpperCase() + str.slice(1) : str
+}
+
+
+ReactDom.render(
+    <Form />,
+    document.getElementById('firstDiv')
+);
