72889002e8f677d37e36685f05c03399d559122b
p.sem <p.sem@skbkontur.ru>
2023-02-27 09:12:28 +0500

    update packages to latest

--- tasks/src/8.FormRow/Toggle.js
+++ tasks/src/8.FormRow/Toggle.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDom from 'react-dom';
 import './toggle.css';
 
 export default class Toggle extends React.Component {
--- tasks/src/9.ColorsOfTime/TimeDisplay.js
+++ tasks/src/9.ColorsOfTime/TimeDisplay.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDom from 'react-dom';
 import PropTypes from 'prop-types';
 import './styles.css';
 
